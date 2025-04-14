dbb3ebd57a8a20d59e53f202b4bf6f37ca907789
Swarnasree-S <saiswarnasree@gmail.com>
2025-04-14 16:26:48 +0530

    Update json_processor.py  mapping individual folder

--- json_processor.py
+++ json_processor.py
@@ -5,13 +5,25 @@ import config_loader
 FOLDER_MAPPING = config_loader.config.get("folder_mapping", {})
 
 def process_dashboard_json(dashboard_json, target_folder_name):
-    dashboard_json["dashboard"]["tags"] = [FOLDER_MAPPING.get(target_folder_name, target_folder_name)]
+    # Fetch mapped folder name or fallback to the target folder name
+    mapped_folder = FOLDER_MAPPING.get(target_folder_name, target_folder_name)
+
+    dashboard_json["dashboard"]["tags"] = [mapped_folder]
     dashboard_json["dashboard"]["links"] = [{
-        "title": target_folder_name,
-        "url": f"/dashboards?folder={target_folder_name}",
+        "title": mapped_folder,
+        "url": f"/dashboards?folder={mapped_folder}",
         "type": "dashboards",
-        "tags": [target_folder_name]
+        "tags": [mapped_folder],
+        "asDropdown": False,
+        "icon": "dashboard",
+        "includeVars": False,
+        "keepTime": False,
+        "targetBlank": False
     }]
+    
+    # Clear the existing ID and generate a new unique ID
     dashboard_json["dashboard"]["id"] = None
     dashboard_json["dashboard"]["uid"] = f"{str(uuid.uuid4())[:8]}_{int(time.time())}"
+
+   
     return dashboard_json
